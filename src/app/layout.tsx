1c034e7fb0a98e2c4040d4cbdc3247ee4ce35b39
TamNgocPham <71805840+ngoctam102@users.noreply.github.com>
2025-08-12 01:44:09 +0700

    config favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -50,21 +50,16 @@ export const metadata: Metadata = {
     },
   },
   viewport: "width=device-width, initial-scale=1",
-  icons: {
-    icon: [
-      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
-      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
-      { url: "/favicon.ico" },
-    ],
-    apple: "/apple-touch-icon.png",
-  },
-  manifest: "/site.webmanifest",
 };
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en">
       <head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
         {/* Robots meta viết tay để có max-image-preview */}
         <meta
           name="robots"
